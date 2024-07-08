daf0eca4984294f6c3dda5d452b5f9c8e87a3b26
Brandon Nicolas Fernandez Rivas <brandon.fernandez2023@twk.cl>
2024-07-08 00:46:02 -0400

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { EmpresaListComponent } from './empresa-list/empresa-list.component';
+import { EmpresaDetailComponent } from './empresa-detail/empresa-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: EmpresaListComponent },
+  { path: 'empresa/:id', component: EmpresaDetailComponent }
+];
